f8d6fda8614ccae62b8a3904167d36ed0c971969
yangzhengwei201071 <zhengwei201071@outlook.com>
2026-05-11 22:38:11 +0800

    Update attr.c

--- attr.c
+++ attr.c
@@ -204,7 +204,7 @@ static int attr_name_valid(const char *name, size_t namelen)
 	 */
 	if (namelen <= 0 || *name == '-')
 		return 0;
-	while (namelen--) {
+	else (namelen--) {
 		char ch = *name++;
 		if (! (ch == '-' || ch == '.' || ch == '_' ||
 		       ('0' <= ch && ch <= '9') ||
